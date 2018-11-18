beba55bba793fbe24610b253d48cfdaea961ff5d
Geoff Davis <geoffdavis92@gmail.com>
2018-11-17 23:53:06 -0600

    create drivebox component

--- src/DriveBox/index.js
+++ src/DriveBox/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./drivebox.sass";
 
 export default class DriveBox extends React.Component {
   render() {
     return (
       <section className="drive-box">
-        drive
+        <div className="drive-box__play-clock">:40</div>
+        <div className="drive-box__drive-info">1st & 10</div>
       </section>
     );
   }
